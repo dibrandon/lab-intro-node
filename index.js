b4ff70fc54570e35185a68702f9662fd7c0f7c20
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-07-18 04:27:47 +0200

    lab intro node terminado

--- index.js
+++ index.js
@@ -1,17 +1,85 @@
-class SortedList {
-  constructor() {}
+const express = require("express");
+const app = express();
+const port = 8008;
 
-  add(item) {}
+app.use(express.static("public")); //todos mis elementos estaticos estan en public
 
-  get(pos) {}
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
 
-  max() {}
+app.get("/home", (req, res) => {
+  res.send("Pagina de Home");
+});
 
-  min() {}
+app.get("/casa", (req, res) => {
+  res.send("Esta es mi casa");
+});
 
-  sum() {}
+app.get("/friends/:friendParam", (req, res) => {
+  console.log(req.params);
+  //res.send("My friends")
+  if (req.params.friendParam === "Victor") {
+    res.send("Información sobre Victor");
+  } else if (req.params.friendParam === "Curro") {
+    res.send("Información sobre Curro");
+  } else {
+    res.send("No tengo mas amigos");
+  }
+});
 
-  avg() {}
-}
+app.get("/speak/:languageParam", (req, res) => {
+  if (req.params.languageParam === "pig") {
+    res.send("The pig says 'Oink!'");
+  } else if (req.params.languageParam === "cow") {
+    res.send("The cow says 'Moo'");
+  } else if (req.params.languageParam === "dog") {
+    res.send("The dog says 'Woof Woof!'");
+  } else {
+    res.send("I dont have that lenguage'");
+  }
+});
 
-module.exports = SortedList;
+app.get("/hola", (req,res) => {
+    res.status(302).send("Esto lo escribi yo");
+})
+
+//greet/hola/3
+
+app.get("/greet/:greetWord/:timesGreet", (req, res) => {
+  const { greetWord, timesGreet } = req.params;
+  let fullMessage = greetWord.repeat(timesGreet);
+  res.send(fullMessage);
+});
+
+app.get("/mi-pagina", (req, res) => {
+  res.sendFile(__dirname + "/views/mi-pagina.html"); //ruta absoluta del elemento
+});
+
+app.get("/sobre-mi", (req, res) => {
+    res.status(200).sendFile(__dirname + "/views/sobre-mi.html"); //ruta absoluta del elemento
+  });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/hola", (req, res) => {
+  console.log(req.params);
+  //res.send("My friends")
+  if (req.params.friendParam === "hola") {
+    res.send("esto lo he escrito yo");
+  } else if (req.params.friendParam === "Curro") {
+    res.send("Información sobre Curro");
+  } else {
+    res.send("No tengo mas amigos");
+  }
+});
+
+//Actividad
+//1.Crea una coleccion de postman que llame a todas los endpoint. hecho
+//2.Crea un endpoint nuevo GET /hola que devuelva: 
+// codigo 302 y 'Esto lo he escrito yo'. hecho
+//3.Crea un endpoint nuevo GET /sobre-mi que devuelva:
+// codigo 200 y la pagina sobre mi que esta en views. hecho
+//Llama con Postman a estos endpoints. hecho
+
